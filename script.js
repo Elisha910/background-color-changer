909bd166b9202e247a42688188c92da3ab9cd95c
Elisha910 <akosonraymond@gmail.com>
2024-12-11 18:15:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let main = document.getElementById("main")
+let black = document.getElementById("black")
+black.addEventListener("click",function changeColor(){
+    main.style.backgroundColor = "black"
+    main.style.color = "white"
+})
+let red = document.getElementById("red")
+red.addEventListener("click",function changeColor(){
+    main.style.backgroundColor = "red"
+     main.style.color = "white"
+})
+let green = document.getElementById("green")
+green.addEventListener("click",function changeColor(){
+    main.style.backgroundColor = "green"
+     main.style.color = "white"
+})
+let blue = document.getElementById("blue")
+blue.addEventListener("click",function changeColor(){
+    main.style.backgroundColor = "blue"
+     main.style.color = "white"
+})
